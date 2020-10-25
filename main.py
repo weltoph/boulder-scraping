69271d790b52b9a5afa6c4866b14e8fe42311364
Christoph Welzel <welzel@in.tum.de>
2020-10-25 14:42:18 +0100

    added simple peewee model

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from urllib import request
 from bs4 import BeautifulSoup  # type: ignore
 
-import sys
-
-if len(sys.argv) != 2:
-    print(f"Usage: {sys.argv[0]} <url>")
+import model
+import config
 
 try:
-    with request.urlopen(sys.argv[1]) as page:
+    with request.urlopen(config.URL) as page:
         if page.code == 200:
             soup = BeautifulSoup(page, "html.parser")
             freimann = soup.find("div", "sys-hall-title", string="KB Freimann")
             parent = freimann.parent
             values = parent.find_all("div", "sys-hall-limit-value")
             boulder = values[-1]
-            value = int(boulder.find("span").text)
-            print(value)
+            value_text = boulder.find("span").text
+            if not value_text:
+                raise ValueError("could not find correct value")
+            else:
+                try:
+                    value = int(value_text)
+                    print(f"logging value: {value}")
+                    scrap = model.Scrap(free_spaces=value, error=None)
+                    scrap.save()
+                except ValueError:
+                    raise ValueError(f"could not convert {value_text} to int")
         else:
-            raise ValueError("Oppps")
-except Exception:
-    # report this via Telegram
-    pass
+            raise ValueError(f"error code: {page.code}")
+except Exception as e:
+    print(f"logging error: {e}")
+    scrap = model.Scrap(error=str(e), free_spaces=None)
+    scrap.save()
---
+++ model.py
@@ -0,0 +1,21 @@
+import peewee  # type: ignore
+import datetime
+
+db = peewee.SqliteDatabase("scraps.db", pragmas={
+    'journal_mode': 'wal',
+    'foreign_keys': 1,
+    'ignore_check_constraints': 0})
+
+
+class Scrap(peewee.Model):
+    date = peewee.DateTimeField(default=datetime.datetime.now)
+    free_spaces = peewee.IntegerField(null=True)
+    error = peewee.CharField(null=True)
+
+    class Meta:
+        database = db
+
+
+def create_tables():
+    with db:
+        db.create_tables([Scrap])
