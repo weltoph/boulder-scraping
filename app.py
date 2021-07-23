f077a50b9e0bb537f146b6e53d7839bf84b08628
Christoph Welzel <welzel@in.tum.de>
2021-07-23 22:25:56 +0200

    data rendering

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from quart import Quart, jsonify, request, abort
 import datetime
+import model
 
 
 app = Quart(__name__)
@@ -30,11 +31,34 @@ async def get_data():
                   description=("Could not parse end parameter. "
                                "Format: YYYY-MM-DD"))
 
+    # Filtering for dates in sql seems to be hard, so we do this in Python.
+    datapoints = list(model.DataPoint.select())
+    errorpoints = list(model.ErrorPoint.select())
+
     return jsonify({
         "beginning": beginning_date,
-        "type_beginning": str(type(beginning_date)),
         "ending": end_date,
-        "type_ending": str(type(end_date))})
+        "data": [
+            {
+                "date": dp.date.isoformat(),
+                "place": str(dp.place),
+                "boulder": int(dp.boulder) if dp.boulder is not None else None,
+                "climbing": (int(dp.climbing)
+                             if dp.climbing is not None
+                             else None)}
+            for dp in model.DataPoint.select()
+            if beginning_date <= datetime.date(dp.date.year,
+                                               dp.date.month,
+                                               dp.date.day) <= end_date],
+        "errors": [
+            {
+                "date": ep.date.isoformat(),
+                "msg": str(ep.msg)}
+            for ep in model.ErrorPoint.select()
+            if beginning_date <= datetime.date(ep.date.year,
+                                                ep.date.month,
+                                                ep.date.day) <= end_date]
+        })
 
 
 if __name__ == "__main__":
